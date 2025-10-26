009352fcca7de50cf29c2e108abb23cae75ef801
Elhoucine Elmourabit <elhoucine.elmourabit.pro@gmail.com>
2025-10-26 15:46:01 +0100

    feat: Implemented a Kafka stream processor that consumes data from the topic T2, processes it and publishes it to the topic T3

--- kafka-spring-cloud/src/main/java/net/houss/kafkaspringcloud/handlers/PageEventHandler.java
+++ kafka-spring-cloud/src/main/java/net/houss/kafkaspringcloud/handlers/PageEventHandler.java
@@ -1,17 +1,21 @@
 package net.houss.kafkaspringcloud.handlers;
 
 import net.houss.kafkaspringcloud.events.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
 
+    // doesn't have an output
     @Bean
     public Consumer<PageEvent> pageEventConsumer(){
         return (input) -> {
@@ -21,6 +25,7 @@ public class PageEventHandler {
         };
     }
 
+    // doesn't have an input
     @Bean
     public Supplier<PageEvent> pageEventSupplier(){
         return () -> {
@@ -33,4 +38,12 @@ public class PageEventHandler {
         };
     }
 
+    // does have an input and an output
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamKStreamFunction(){
+        return input -> {
+            return input.filter((k, v) -> v.duration() > 100)
+                    .map((k, v) -> new KeyValue<>(v.name(), v.duration()));
+        };
+    }
 }
